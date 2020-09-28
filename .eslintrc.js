9f7f1cde0024b8a328a2c4d3ddadb4901115b27b
Bogdan Luca <luca.bogdan@gmail.com>
2020-09-28 17:58:02 +0300

    Final version.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
+function disableInDevelopment(rules) {
+  return rules.reduce((acc, rule) => {
+    // warn in development, error in production
+    // to completely disable in development, use 'off' instead of 'warn'
+    acc[rule] = process.env.NODE_ENV === 'production' ? 'error' : 'warn';
+    return acc;
+  }, {});
+}
+
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ['plugin:vue/recommended', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    // add other annoying rules to the array
+    ...disableInDevelopment(['no-console', 'no-debugger', 'no-unused-vars']),
+    // other custom rules here
+  },
+};
